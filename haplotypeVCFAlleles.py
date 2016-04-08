cf884ebc80922d0907fc4baa2d58c033ab3e4929
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2016-04-08 19:10:54 +0100

    Create haplotypeVCFAlleles.py

---
+++ haplotypeVCFAlleles.py
@@ -0,0 +1,37 @@
+import vcf 
+import os
+vcf_mat = vcf.Reader(open('131.maternal.vcf','r'))
+vcf_mat_out = vcf.Writer(open('131.maternal2.vcf','w'),vcf_mat)
+
+for record in vcf_mat:
+	if record.genotype('131')['GT'].split('|')[1] == '1':
+		tmp=record
+		A1=tmp.REF
+		A2=tmp.ALT
+		tmp.REF=str(A2[0])
+		tmp.ALT=[vcf.model._Substitution(A1)]
+		vcf_mat_out.write_record(tmp)
+	else:
+		vcf_mat_out.write_record(record)
+
+vcf_mat_out.close()
+os.system('mv 131.maternal2.vcf 131.maternal.vcf')
+
+import vcf 
+
+vcf_pat = vcf.Reader(open('131.paternal.vcf','r'))
+vcf_pat_out = vcf.Writer(open('131.paternal2.vcf','w'),vcf_pat)
+
+for record in vcf_pat:
+	if record.genotype('131')['GT'].split('|')[0] == '1':
+		tmp=record
+		A1=tmp.REF
+		A2=tmp.ALT
+		tmp.REF=str(A2[0])
+		tmp.ALT=[vcf.model._Substitution(A1)]
+		vcf_pat_out.write_record(tmp)
+	else:
+		vcf_pat_out.write_record(record)
+
+vcf_pat_out.close()
+os.system('mv 131.paternal2.vcf 131.paternal.vcf')
