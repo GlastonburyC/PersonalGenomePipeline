8a9539c74cdce6479e568ca6b80bc9d3ed2d72c8
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2016-06-12 20:55:53 +0100

    Update workflow.sh

--- workflow.sh
+++ workflow.sh
@@ -168,4 +168,4 @@ java -jar ~/scratch/software/GenomeAnalysisTK.jar -R "$SAMPLE_ID"/paternal/"$SAM
 python ../software/PersonalGenomePipeline/ASERefCord.py "$SAMPLE_ID"/maternal/"$SAMPLE_ID".ASE.mat.csv "$SAMPLE_ID"/paternal/"$SAMPLE_ID".ASE.pat.csv "$SAMPLE_ID"/"$SAMPLE_ID".hets.GATK.sorted.vcf "$SAMPLE_ID"/maternal/"$SAMPLE_ID".maternal.vcf "$SAMPLE_ID"/paternal/"$SAMPLE_ID".paternal.vcf "$SAMPLE_ID".maternal.alleles.csv "$SAMPLE_ID".paternal.alleles.csv "$SAMPLE_ID"/maternal/"$SAMPLE_ID".maternal.ref.csv "$SAMPLE_ID"/paternal/"$SAMPLE_ID".paternal.ref.csv
 #################################
 
-R CMD BATCH ASERefOut.R --args "$SAMPLE_ID".maternal.ref.csv "$SAMPLE_ID".paternal.ref.csv "$SAMPLE_ID".ASE.csv
+Rscript ../software/PersonalGenomePipeline/ASERefOut.R "$SAMPLE_ID"/maternal/"$SAMPLE_ID".maternal.ref.csv "$SAMPLE_ID"/paternal/"$SAMPLE_ID".paternal.ref.csv "$SAMPLE_ID"/"$SAMPLE_ID".ASE.csv
