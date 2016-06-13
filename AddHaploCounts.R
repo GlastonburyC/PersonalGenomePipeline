da207fd285c1d8815ddc6202ef6fc138832a62c9
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2016-06-13 01:07:06 +0100

    Update AddHaploCounts.R

--- AddHaploCounts.R
+++ AddHaploCounts.R
@@ -12,5 +12,5 @@ mat[,7]=mat[,7]+pat[,7]
 
 colnames(mat)[7]=SAMPLE_ID
 
-file_out=paste(SAMPLE_ID,".GeneCount.Final.txt",sep="")
+file_out=args[4]
 write.table(mat,file_out,col.names=T,row.names=F,sep="\t",quote=F))
