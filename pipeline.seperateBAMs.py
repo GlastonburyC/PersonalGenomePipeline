4b72743f5a62ab718639ba04a17eb5ba77157566
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2016-06-17 23:24:01 +0100

    Update pipeline.seperateBAMs.py

--- pipeline.seperateBAMs.py
+++ pipeline.seperateBAMs.py
@@ -263,7 +263,7 @@ vcf_writer = vcf.Writer(open(sys.argv[1]+'/'+sys.argv[1]+'.hets.phased.vcf','w')
 for record in vcf_reader:
   if len(record.get_hets()) != 0:
   	if record.genotype(ID)['GT'][1]=='|':
-  		if record.QUAL >= 30:
+  		if sorted(record.genotype(ID)['PL'])[1] >= 30:
   			vcf_writer.write_record(record)
 vcf_writer.close()
 
