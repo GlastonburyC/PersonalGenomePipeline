6de77e7e2dd1e3f523dcca31096be4d02dda8e40
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2016-07-19 15:37:09 +0100

    Update filterVariantGQ.py

--- filterVariantGQ.py
+++ filterVariantGQ.py
@@ -7,5 +7,4 @@ vcf_writer = vcf.Writer(open(sys.argv[1]+'/'+sys.argv[1]+'.vcf.gz2','w'),vcf_rea
 
 for record in vcf_reader:
   if record.genotype(ID)['GT'][1]=='|':
-    if sorted(record.genotype(ID)['PL'])[1] >= 30:
-      vcf_writer.write_record(record)
+    vcf_writer.write_record(record)
