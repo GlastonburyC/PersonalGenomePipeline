5fe2dadb630e00aea646186848beacad41b3bd40
Craig A. Glastonbury <craig.glastonbury@kcl.ac.uk>
2016-07-28 12:34:32 +0100

    Update move.sh

--- move.sh
+++ move.sh
@@ -13,16 +13,16 @@ while read line ; do
 
 SAMPLE_ID=$line
 
-if [ ! -d /media/data/blood_samples/"$SAMPLE_ID" ]; then
+if [ ! -d /media/shared_data/data/blood_samples/"$SAMPLE_ID" ]; then
 
-	mkdir /media/data/blood_samples/"$SAMPLE_ID"
-	mkdir /media/data/blood_samples/"$SAMPLE_ID"/reference
+	mkdir /media/shared_data/data/blood_samples/"$SAMPLE_ID"
+	mkdir /media/shared_data/data/blood_samples/"$SAMPLE_ID"/reference
 
-	mv /media/data/BLOOD/"$SAMPLE_ID"_sorted.bam /media/data/blood_samples/"$SAMPLE_ID"/"$SAMPLE_ID"_sorted.bam
+	mv /media/shared_data/data/BLOOD/"$SAMPLE_ID"_sorted.bam /media/shared_data/data/blood_samples/"$SAMPLE_ID"/"$SAMPLE_ID"_sorted.bam
 
 else
 
-mv /media/data/BLOOD/"$SAMPLE_ID"_sorted.bam /media/data/blood_samples/"$SAMPLE_ID"/"$SAMPLE_ID"_sorted.bam
+mv /media/shared_data/data/BLOOD/"$SAMPLE_ID"_sorted.bam /media/shared_data/data/blood_samples/"$SAMPLE_ID"/"$SAMPLE_ID"_sorted.bam
 
 fi
 
